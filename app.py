eb2f6944c06b887e52088d09294c3efb57464a88
Pavvlovv <kriger.it@yandex.ru>
2025-06-29 18:41:28 +0700

    Refactor: Implement multi-stage build and env config

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask
 import os
 import socket
 
-# Создаем экземпляр приложения Flask
 app = Flask(__name__)
 
-# Определяем маршрут для корневого URL ('/')
 @app.route('/')
 def hello():
-    # Получаем имя хоста, чтобы видеть, где мы работаем
     hostname = socket.gethostname()
     return f"Hello, Alex! I'm running inside a container on host: {hostname}\n"
 
-# Условие для запуска сервера при выполнении скрипта напрямую
 if __name__ == "__main__":
-    # Запускаем веб-сервер на всех сетевых интерфейсах ('0.0.0.0')
-    # Это обязательно для доступа к контейнеру извне.
-    # Порт 8080 будет использоваться внутри контейнера.
-    app.run(host='0.0.0.0', port=8080)
+    # Получаем порт из переменной окружения APP_PORT.
+    # Если переменная не задана, используем 8080 по умолчанию.
+    port = int(os.environ.get('APP_PORT', '8080'))
+    
+    # Запускаем сервер на указанном порту
+    app.run(host='0.0.0.0', port=port)
